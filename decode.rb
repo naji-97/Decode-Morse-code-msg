ae1c97eca281d46a7fff65e00ca88e4db4448948
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-14 14:03:45 +0000

    Add code to decode entire morse message

--- decode.rb
+++ decode.rb
@@ -1,5 +1,5 @@
 CODES = {
-  '.-'  => 'A',
+  '.-' => 'A',
   '-...' => 'B',
   '-.-.' => 'C',
   '-..' => 'D',
@@ -25,20 +25,27 @@ CODES = {
   '-..-' => 'X',
   '-.--' => 'Y',
   '--..' => 'Z'
-}
+}.freeze
 
 def decode_char(char)
   CODES[char].upcase
 end
 
-def decode_word(word)
-  morse_chars = word.split(' ')
-  decoded_chars = morse_chars.map { |morse_char| decode_char(morse_char) }
-  decoded_chars.join('')
-
-  
-end
-puts decode_word("-- -.--   -. .- -- .")
-
-
 puts decode_char('.-')
+
+def decode_word(word)
+  morse_chars = word.split
+  decoded_chars = morse_chars.map { |morse_char| decode_char(morse_char) }
+  decoded_chars.join
+end
+
+puts decode_word('-- -.--')
+
+def decode_message(morse_message)
+  morse_words = morse_message.split('   ')
+  decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
+  decoded_words.join(' ')
+end
+
+puts decode_message('-- -.--   -. .- -- .')
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
